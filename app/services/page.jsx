3192644f78914a6db113e1aa5f2a359daff4e2f0
Deepanshu8560 <gupta.d.deepanshu@gmail.com>
2024-10-01 18:51:49 +0530

    Update page.jsx

--- app/services/page.jsx
+++ app/services/page.jsx
@@ -29,23 +29,23 @@ import { motion } from "framer-motion";
 
 const Services = () => {
   return (
-    <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
+    <section className="min-h-[80vh] flex flex-col justify-center py-8 xl:py-0">
       <div className="container mx-auto ">
         <motion.div initial={{opacity: 0}} animate={{opacity: 1, transition:{delay:2.4, duration:0.4, ease:"easeIn"},
         
-      }}className="grid grid-cols-1 md:grid-cols-2 gap=[60px]">
+      }}className="grid grid-cols-1 md:grid-cols-2 gap=[50px]">
         {services.map((service, index)=> {
           return (
-            <div key={index} className="flex-1 flex flex-col justify-center gap-6 group">
-              <div className="w-full flex justify-between items-center">
+            <div key={index} className="flex-1 flex flex-col justify-center gap-8 group">
+              <div className="w-[90%] flex justify-between items-center">
                 <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500" >{service.num}</div>
-                <Link href={service.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45 ">
-                <BsArrowDownRight className="text-primary text-3xl " />
+                <Link href={service.href} className="w-[60px] h-[60px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45 ">
+                <BsArrowDownRight className="text-primary text-2xl " />
                 </Link>
               </div>
               <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">{service.title}</h2>
               <p className="text-white/60  ">{service.description}</p>
-              <div className="border-b border-white/20 w-full"></div>
+              <div className="border-b border-white/20 w-[90%]"></div>
             </div>
           );
         })}
