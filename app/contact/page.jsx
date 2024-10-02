a62a6c8d566a51d309265f9af20792e6406c6101
Deepanshu8560 <gupta.d.deepanshu@gmail.com>
2024-10-02 21:23:19 +0530

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -41,7 +41,7 @@ const Contact = () => {
         <div className="xl:w-[54%] order-2 xl:order-none">
           <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl ">
             <h3 className="text-4xl text-accent ">Let's Work Together</h3>
-            <p className="text-white/60">fa adf af af af df dg adgd gds g sfg fdh fdh gdh gdh dgh dhd hd d fdg fdg sfdg sfdg </p>
+            
             <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
               <Input type="firstname" placeholder="Firstname" />
               <Input type="fLastname" placeholder="Lastname" />
